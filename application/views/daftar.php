3ba89c3978406773d119c231b93ed5b167b36715
RafaelNixon <62681554+RafaelNixon@users.noreply.github.com>
2020-12-27 20:33:44 +0700

    Update daftar.php

--- application/views/daftar.php
+++ application/views/daftar.php
@@ -1,115 +1,233 @@
 <!--Pendaftaran Start-->
-      <div class="pendaftaran">  
+<div class="pendaftaran">
 
-        <!--Pendaftaran Banner Start-->
-        <div class="row">
-          <div class="col s10 offset-s1">
-            <div class="card black">
-              <div class="card-content white-text">
-                <span class="card-title center-align">Pendaftaran ARA 2021</span>
-                <p class="center-align">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                <p class="center-align">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p><br>
-              </div>
-              <div class="card-image">
-                <div class="row">
-                  <div class="col s12 l4">
-                    <img class="responsive-img" src="<?php echo base_url(); ?>assets/images/banner1.png">
-                    <p class="card-text-home center-align">Lorem Ipsum Dolor</p>
-                  </div>
-                  <div class="col s12 l4">
-                    <img class="responsive-img" src="<?php echo base_url(); ?>assets/images/banner2.png">
-                    <p class="card-text-home center-align">Lorem Ipsum Dolor</p>
-                  </div>
-                  <div class="col s12 l4">
-                    <img class="responsive-img" src="<?php echo base_url(); ?>assets/images/banner3.png">
-                    <p class="card-text-home center-align">Lorem Ipsum Dolor</p>
-                  </div>
-                </div>
-              </div>
+  <!--Pendaftaran Banner Start-->
+  <div class="row">
+    <div class="col s10 offset-s1">
+      <div class="card black">
+        <div class="card-content white-text">
+          <span class="card-title center-align">Pendaftaran ARA 2021</span>
+          <!-- <p class="center-align">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+            qui officia deserunt mollit anim id est laborum."</p>
+          <p class="center-align">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+            qui officia deserunt mollit anim id est laborum."</p><br> -->
+        </div>
+        <!-- <div class="card-image">
+          <div class="row">
+            <div class="col s12 l4">
+              <img class="responsive-img" src="<?php echo base_url(); ?>assets/images/banner1.png">
+              <p class="card-text-home center-align">Lorem Ipsum Dolor</p>
+            </div>
+            <div class="col s12 l4">
+              <img class="responsive-img" src="<?php echo base_url(); ?>assets/images/banner2.png">
+              <p class="card-text-home center-align">Lorem Ipsum Dolor</p>
+            </div>
+            <div class="col s12 l4">
+              <img class="responsive-img" src="<?php echo base_url(); ?>assets/images/banner3.png">
+              <p class="card-text-home center-align">Lorem Ipsum Dolor</p>
             </div>
           </div>
-        </div>
-        <!--Pendaftaran Banner Ends-->
-
-        <!--Pendaftaran Event List Starts-->
-        <div class="row">
-          <div class="col s10 offset-s1">
-            <div class="card black">
-              <div class="row">
-                <div class="col s12 l2">
-                  <p class="card-text-event center-align"><strong>Our Event</strong></p>
-                </div>
-                <div class="col s12 l5">
-                  <div class="card">
-                    <div class="card-image waves-effect waves-block waves-light">
-                      <img class="activator" src="<?php echo base_url(); ?>assets/images/banner1.png">
-                    </div>
-                    <div class="card-content">
-                      <span class="card-title activator grey-text text-darken-4">Webinar</span>
-                      <p>Click to find out more</p>
-                    </div>
-                    <div class="card-reveal">
-                      <span class="card-title grey-text text-darken-4">Webinar<i class="material-icons right">close</i></span>
-                      <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                      <p><a href="<?php echo base_url(); ?>pendaftaran/webinar">Daftar Webinar Sekarang</a></p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col s12 l5">
-                  <div class="card">
-                    <div class="card-image waves-effect waves-block waves-light">
-                      <img class="activator" src="<?php echo base_url(); ?>assets/images/banner2.png">
-                    </div>
-                    <div class="card-content">
-                      <span class="card-title activator grey-text text-darken-4">Internet of Things</span>
-                      <p>Click to find out more</p>
-                    </div>
-                    <div class="card-reveal">
-                      <span class="card-title grey-text text-darken-4">Internet of Things<i class="material-icons right">close</i></span>
-                      <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                      <p><a href="<?php echo base_url(); ?>pendaftaran/iot">Daftar Internet of Things Sekarang</a></p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col s12 l5 offset-l2">
-                  <div class="card">
-                    <div class="card-image waves-effect waves-block waves-light">
-                      <img class="activator" src="<?php echo base_url(); ?>assets/images/banner1.png">
-                    </div>
-                    <div class="card-content">
-                      <span class="card-title activator grey-text text-darken-4">Olimpiade</span>
-                      <p>Click to find out more</p>
-                    </div>
-                    <div class="card-reveal">
-                      <span class="card-title grey-text text-darken-4">Olimpiade<i class="material-icons right">close</i></span>
-                      <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                      <p><a href="<?php echo base_url(); ?>pendaftaran/olimpiade">Daftar Olimpiade Sekarang</a></p>
-                    </div>
-                  </div>
-                </div>
-                <div class="col s12 l5">
-                  <div class="card">
-                    <div class="card-image waves-effect waves-block waves-light">
-                      <img class="activator" src="<?php echo base_url(); ?>assets/images/banner2.png">
-                    </div>
-                    <div class="card-content">
-                      <span class="card-title activator grey-text text-darken-4">Capture The Flag</span>
-                      <p>Click to find out more</p>
-                    </div>
-                    <div class="card-reveal">
-                      <span class="card-title grey-text text-darken-4">Capture the Flag<i class="material-icons right">close</i></span>
-                      <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                      <p><a href="<?php echo base_url(); ?>pendaftaran/ctf">Daftar Capture The Flag Sekarang</a></p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!--Pendaftaran Event List Ends-->
-
+        </div> -->
       </div>
-      <!--Pendaftaran Ends-->
+    </div>
+  </div>
+  <!--Pendaftaran Banner Ends-->
+
+  <!--Pendaftaran Event List Starts-->
+  <div class="row">
+    <div class="col s10 offset-s1">
+      <div class="card black">
+        <div class="row">
+          <div class="col s12 l2">
+            <p class="card-text-event center-align"><strong>Our Event</strong></p>
+          </div>
+          <div class="col s12 l5">
+            <div class="card">
+              <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="<?php echo base_url(); ?>assets/images/banner1.png">
+              </div>
+              <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4">Webinar</span>
+                <p>Click to find out more</p>
+              </div>
+              <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Webinar<i class="material-icons right">close</i></span>
+                <p><b>Pendaftaran</b></p>
+                Tidak ada syarat pendaftaran
+                <p><a href="<?php echo base_url(); ?>pendaftaran/webinar">Daftar Webinar Sekarang</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col s12 l5">
+            <div class="card">
+              <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="<?php echo base_url(); ?>assets/images/banner2.png">
+              </div>
+              <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4">Internet of Things</span>
+                <p>Click to find out more</p>
+              </div>
+              <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Internet of Things<i
+                    class="material-icons right">close</i></span>
+                <p><b>Pendaftaran</b></p>
+                Periode pendaftaran : 25 Desember 2020 - 24 Januari 2021 pukul 23:59 WIB.
+                <ol>
+                  <li>
+                    Perwakilan tim mengisi formulir pendaftaran pada situs <a
+                      href="<?php echo base_url(); ?>pendaftaran/iot">https://arek.its.ac.id/pendaftaran/iot</a> dan
+                    mengisi data diri seluruh anggota dengan lengkap.
+                  </li>
+                  <li>
+                    Pada saat mengisi formulir, perwakilan tim perlu melengkapi berkas-berkas pendaftaran sebagai
+                    berikut:
+                    <ol>
+                      <li>
+                        Foto/scan Kartu Tanda Mahasiswa setiap anggota.
+                      </li>
+                      <li>
+                        Surat Keterangan Mahasiswa Aktif / KRS
+                      </li>
+                    </ol>
+                  </li>
+                  <li>
+                    Tim yang telah mengisi dan melengkapi formulir pendaftaran akan diverifikasi oleh panitia dalam
+                    rentang waktu 2 x 24 (dua kali dua puluh empat) jam.
+                  </li>
+                  <li>
+                    Tim yang telah diverifikasi secara resmi menjadi peserta Lomba KTI Internet of Things ARA ITS
+                    2021.
+                  </li>
+                </ol>
+                <p><a href="<?php echo base_url(); ?>pendaftaran/iot">Daftar Internet of Things Sekarang</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col s12 l5 offset-l2">
+            <div class="card">
+              <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="<?php echo base_url(); ?>assets/images/banner1.png">
+              </div>
+              <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4">Olimpiade</span>
+                <p>Click to find out more</p>
+              </div>
+              <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Olimpiade<i
+                    class="material-icons right">close</i></span>
+                <p><b>Pendaftaran</b></p>
+                Periode pendaftaran : 25 Desember 2020 - 12 Maret 2021 pukul 23:59 WIB.
+                <ol>
+                  <li>
+                    Perwakilan tim mengisi formulir pendaftaran pada situs <a
+                      href="<?php echo base_url(); ?>pendaftaran/olimpiade">https://arek.its.ac.id/pendaftaran/olimpiade</a>
+                    dan
+                    mengisi data diri
+                    seluruh anggota dengan lengkap.
+                  </li>
+                  <li>
+                    Pada saat mengisi formulir, perwakilan tim perlu melengkapi berkas-berkas pendaftaran sebagai
+                    berikut:
+                    <ol>
+                      <li>
+                        Foto/scan 3x4 setiap anggota.
+                      </li>
+                      <li>
+                        Foto/scan kartu pelajar setiap anggota.
+                      </li>
+                    </ol>
+                  </li>
+                  <li>
+                    Tim yang telah mengisi dan melengkapi formulir pendaftaran akan diverifikasi oleh panitia dalam
+                    rentang
+                    waktu 2 x 24 (dua kali dua puluh empat) jam.
+                  </li>
+                  <li>
+                    Peserta dapat melakukan pembayaran biaya pendaftaran sesuai dengan nominal biaya pendaftaran pada
+                    tiap gelombang ke rekening berikut:
+                    <ul>
+                      <li>
+                        No. Rek : 0254251568
+                      </li>
+                      <li>
+                        Bank : BNI
+                      </li>
+                      <li>
+                        a.n : Muhammad Hilmi Ramadhan
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    Tim yang telah diverifikasi secara resmi menjadi peserta Olimpiade Cyber Security dan IoT ARA 2021.
+                  </li>
+                </ol>
+                <p><a href="<?php echo base_url(); ?>pendaftaran/olimpiade">Daftar Olimpiade Sekarang</a></p>
+              </div>
+            </div>
+          </div>
+          <div class="col s12 l5">
+            <div class="card">
+              <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="<?php echo base_url(); ?>assets/images/banner2.png">
+              </div>
+              <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4">Capture The Flag</span>
+                <p>Click to find out more</p>
+              </div>
+              <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Capture the Flag<i
+                    class="material-icons right">close</i></span>
+                <p><b>Pendaftaran</b></p>
+                Periode pendaftaran : 25 Desember 2020 - 1 Maret 2021, pukul 23.59 WIB.
+                <ol>
+                  <li>
+                    Peserta diwajibkan untuk mengisi setiap data yang ada pada situs resmi ARA 2021 yaitu
+                    <a href="<?php echo base_url(); ?>pendaftaran/ctf">https://arek.its.ac.id/pendaftaran/ctf</a>
+                  </li>
+                  <li>
+                    Peserta dapat melakukan pembayaran biaya pendaftaran sesuai dengan nominal biaya pendaftaran pada
+                    tiap
+                    gelombang ke rekening berikut:
+                    <ul>
+                      No. Rek : 0254251568
+                    </ul>
+                    <ul>
+                      Bank : BNI
+                    </ul>
+                    <ul>
+                      a.n : Muhammad Hilmi Ramadhan
+                    </ul>
+                  </li>
+                  <li>
+                    Tim yang telah melengkapi berkas pendaftaran akan diverifikasi oleh panitia dalam rentang waktu 2x24
+                    (empat puluh delapan) jam. Apabila tim belum diverifikasi dalam rentang waktu tersebut, peserta
+                    dapat
+                    menghubungi panitia melalui Contact Person yang tersedia.
+                  </li>
+                  <li>
+                    Tim yang telah terverifikasi akan mendapatkan email yang berisi username dan password untuk memasuki
+                    situs lomba. Bagi tim yang sudah mendaftar tetapi tidak mendapatkan email sampai tanggal 5 Maret
+                    2021 dapat menghubungi panitia melalui Contact Person yang tersedia.
+                  </li>
+                </ol>
+
+                <p><a href="<?php echo base_url(); ?>pendaftaran/ctf">Daftar Capture The Flag Sekarang</a></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--Pendaftaran Event List Ends-->
+
+</div>
+<!--Pendaftaran Ends-->
