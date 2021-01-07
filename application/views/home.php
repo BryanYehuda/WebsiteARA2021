f1cb807bcdb32bb1b3f833a48b7d482d9af27830
Bryan Yehuda <bryanyehuda@gmail.com>
2021-01-08 00:02:09 +0700

    y

--- application/views/home.php
+++ application/views/home.php
@@ -14,8 +14,116 @@
         </div>
 
         <div class="content-designed">
-            <!--Content Here-->
-            Konten
+            <div class="row">
+                <div class="col s12">
+                    <span class="form-title-designed center-align"><strong>Apa itu ARA 2021?</strong></span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s12" style="text-align:justify">
+                    <p>ARA (A Renewal Agent) adalah sebuah kegiatan tahunan yang diselenggarakan oleh Himpunan Mahasiswa Teknologi Informasi ITS 
+                    yang mewadahi para generasi muda, khususnya para mahasiswa dan siswa SMA/Sederajat untuk menyalurkan minat bakat di bidang 
+                    Internet of Things dan Cyber Security. Sesuai dengan namanya, A Renewal Agent,  ARA mengusung tema “For the Golden Generation 
+                    of Indonesia” dimana ARA akan menjadi ajang untuk pencarian agen-agen pembaruan generasi milenial sebagai pilar di bidang 
+                    Teknologi dan Informasi untuk Indonesia 2045. Dengan tagar #SemangatMembARA2021 dan #ARenewalAgentforIndonesia2045 diharapkan 
+                    ARA dapat melahirkan agen-agen pembaruan yang memiliki semangat membara sehingga kedepannnya dapat mendukung perkembangan dan 
+                    kemajuan Indonesia khususnya di bidang Teknologi dan Informasi.</p>
+                </div>  
+            </div>
         </div>
+        <br><br>
+
+        <div class="header-designed">
+            <ul style="margin: 0;">
+                <li class="button-circle-designed" style="background-color: #AC8FBC;">
+                </li>
+                <li class="button-circle-designed" style="background-color: #AAD8B6;">
+                </li>
+                <li class="button-circle-designed" style="background-color: #E7A996;">
+                </li>
+            </ul>
+        </div>
+
+        <div class="content-designed">
+            <div class="row">
+                <div class="col s12">
+                    <span class="form-title-designed center-align"><strong>Event yang ada di ARA 2021</strong></span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s12" style="text-align:justify">
+                    <p>ARA (A Renewal Agent) adalah sebuah kegiatan tahunan yang diselenggarakan oleh Himpunan Mahasiswa Teknologi Informasi ITS 
+                    yang mewadahi para generasi muda, khususnya para mahasiswa dan siswa SMA/Sederajat untuk menyalurkan minat bakat di bidang 
+                    Internet of Things dan Cyber Security. Sesuai dengan namanya, A Renewal Agent,  ARA mengusung tema “For the Golden Generation 
+                    of Indonesia” dimana ARA akan menjadi ajang untuk pencarian agen-agen pembaruan generasi milenial sebagai pilar di bidang 
+                    Teknologi dan Informasi untuk Indonesia 2045. Dengan tagar #SemangatMembARA2021 dan #ARenewalAgentforIndonesia2045 diharapkan 
+                    ARA dapat melahirkan agen-agen pembaruan yang memiliki semangat membara sehingga kedepannnya dapat mendukung perkembangan dan 
+                    kemajuan Indonesia khususnya di bidang Teknologi dan Informasi.</p>
+                </div>  
+            </div>
+        </div>
+        <br><br>
+
+        <div class="header-designed">
+            <ul style="margin: 0;">
+                <li class="button-circle-designed" style="background-color: #AC8FBC;">
+                </li>
+                <li class="button-circle-designed" style="background-color: #AAD8B6;">
+                </li>
+                <li class="button-circle-designed" style="background-color: #E7A996;">
+                </li>
+            </ul>
+        </div>
+
+        <div class="content-designed">
+            <div class="row">
+                <div class="col s12">
+                    <span class="form-title-designed center-align"><strong>Timeline ARA 2021</strong></span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s12" style="text-align:justify">
+                    <p>ARA (A Renewal Agent) adalah sebuah kegiatan tahunan yang diselenggarakan oleh Himpunan Mahasiswa Teknologi Informasi ITS 
+                    yang mewadahi para generasi muda, khususnya para mahasiswa dan siswa SMA/Sederajat untuk menyalurkan minat bakat di bidang 
+                    Internet of Things dan Cyber Security. Sesuai dengan namanya, A Renewal Agent,  ARA mengusung tema “For the Golden Generation 
+                    of Indonesia” dimana ARA akan menjadi ajang untuk pencarian agen-agen pembaruan generasi milenial sebagai pilar di bidang 
+                    Teknologi dan Informasi untuk Indonesia 2045. Dengan tagar #SemangatMembARA2021 dan #ARenewalAgentforIndonesia2045 diharapkan 
+                    ARA dapat melahirkan agen-agen pembaruan yang memiliki semangat membara sehingga kedepannnya dapat mendukung perkembangan dan 
+                    kemajuan Indonesia khususnya di bidang Teknologi dan Informasi.</p>
+                </div>  
+            </div>
+        </div>
+        <br><br>
+
+        <div class="header-designed">
+            <ul style="margin: 0;">
+                <li class="button-circle-designed" style="background-color: #AC8FBC;">
+                </li>
+                <li class="button-circle-designed" style="background-color: #AAD8B6;">
+                </li>
+                <li class="button-circle-designed" style="background-color: #E7A996;">
+                </li>
+            </ul>
+        </div>
+
+        <div class="content-designed">
+            <div class="row">
+                <div class="col s12">
+                    <span class="form-title-designed center-align"><strong>Tentang HMIT</strong></span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s12" style="text-align:justify">
+                    <p>Himpunan Mahasiswa Teknologi Informasi atau HMIT adalah salah satu Organisasi Mahasiswa tingkat departemen yang berada di 
+                    Institut Teknologi Sepuluh Nopember. Tujuan didirikannya HMIT adalah untuk mewujudkan mahasiswa Departemen Teknologi Informasi 
+                    FTEIC ITS sebagai insan terdidik yang unggul dan profesional khususnya dalam pengembangan dan penerapan teknologi informasi, 
+                    serta memiliki tanggung jawab sosial. Setelah didirikan dan disahkan oleh KM ITS pada akhir tahun 2019, saat ini HMIT memiliki 
+                    sebuah kabinet bernama Kabinet Cipta Karya. Nama Cipta Karya sendiri diambil dari kata: “Cipta” yang berarti sebuah kemampuan 
+                    untuk menciptakan sesuatu yang baru, angan-angan yang kreatif; dan “Karya” yang memiliki arti sebuah pekerjaan, hasil perbuatan, 
+                    dan ciptaan. Hingga saat ini, HMIT memiliki big event bernama ARA dan juga ExploIT yang diadakan secara rutin setiap tahunnya.</p>
+                </div>  
+            </div>
+        </div>
+
     </div>
 </div>
