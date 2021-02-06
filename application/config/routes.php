1b2a6248210f92f2d545a938270ec1a3a2124ce1
DidaPrasetyo <didaprasetyorahmat@gmail.com>
2021-02-07 02:21:52 +0700

    add sukseswebinar route

--- application/config/routes.php
+++ application/config/routes.php
@@ -56,6 +56,7 @@ $route['translate_uri_dashes'] = FALSE;
 
 $route['pendaftaran']				=	'welcome/pendaftaran';
 $route['pendaftaran/sukses']		=	'welcome/sukses';
+$route['pendaftaran/sukseswebinar']		=	'welcome/sukseswebinar';
 
 $route['pendaftaran/webinar']		=	'welcome/webinar';
 $route['pendaftaran/iot']			=	'welcome/iot';
