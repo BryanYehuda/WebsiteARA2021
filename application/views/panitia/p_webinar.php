2f14710e8dee68eb5fdb42b117c4e109e3964954
RafaelNixon <62681554+RafaelNixon@users.noreply.github.com>
2021-02-14 22:41:15 +0700

    Admin Webinar

--- application/views/panitia/p_webinar.php
+++ application/views/panitia/p_webinar.php
@@ -6,7 +6,7 @@
                 <th>Institusi</th>
                 <th>Email</th>
                 <th>No. WhatsApp</th>
-                <th>Bukti</th>
+                <th>Sesi</th>
                 <th>Tgl. Daftar</th>
             </tr>
         </thead>
@@ -14,7 +14,9 @@
             <?php foreach($datas as $data) {?>
             <tr draggable="false">
                 <td draggable="false">
-                    <?= $data->nama; ?>
+                    <a data-toggle="modal" data-target="#modal_<?php echo $data->id; ?>_webinar" draggable="false">
+                        <?=$data->nama; ?>
+                    </a>
                 </td>
                 <td draggable="false">
                     <?= $data->institusi; ?>
@@ -26,12 +28,7 @@
                     <?= $data->no_wa; ?>
                 </td>
                 <td draggable="false">
-                    <!-- <a href="<?php echo base_url('uploads/webinar/') . $data->bukti; ?>" target="_blank"
-                        draggable="false"> -->
-                    <a href="<?php echo base_url('uploads/webinar/') . $data->bukti; ?>" target="_blank"
-                        draggable="false">
-                        <?= $data->bukti; ?>
-                    </a>
+                    <?= $data->sesi; ?>
                 </td>
                 <td draggable="false">
                     <?= $data->webinar_timestamp; ?>
@@ -40,5 +37,67 @@
             <?php } ?>
         </tbody>
     </table>
+
+    <?php foreach($datas as $data) {?>
+        <div id="modal_<?php echo $data->id; ?>_webinar" class="modal">
+        <div class="modal-dialog">
+
+            <div class="modal-content" style="background-color: rgb(56, 56, 56);">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">
+                        <?= $data->nama; ?>
+                    </h4>
+                </div>
+                <div class="modal-body">
+                    <p>
+                        <b>
+                            Bukti 1
+                        </b>
+                        <br>
+                            <a href="<?php echo base_url('uploads/webinar/') . $data->nama . '/' . $data->bukti; ?>" target="_blank"
+                            draggable="false">
+                                <?= $data->bukti; ?>
+                            </a>
+                        <br>
+                        <br>
+                        <b>
+                            Bukti 2
+                        </b>
+                        <br>
+                        <a href="<?php echo base_url('uploads/webinar/') . $data->nama . '/' . $data->bukti2; ?>" target="_blank"
+                            draggable="false">
+                                <?= $data->bukti2; ?>
+                            </a>
+                        <br>
+                        <br>
+                        <b>
+                            Bukti Telkom 1
+                        </b>
+                        <br>
+                            <a href="<?php echo base_url('uploads/webinar/') . $data->nama . '/' . $data->bukti_telkom1; ?>" target="_blank"
+                            draggable="false">
+                                <?= $data->bukti_telkom1; ?>
+                            </a>
+                        <br>
+                        <br>
+                            Bukti Telkom 2
+                        </b>
+                        <br>
+                            <a href="<?php echo base_url('uploads/webinar/') . $data->nama . '/' . $data->bukti_telkom2; ?>" target="_blank"
+                            draggable="false">
+                                <?= $data->bukti_telkom2; ?>
+                            </a>
+                        <br>
+
+                    </p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php } ?>
 
 </div>
